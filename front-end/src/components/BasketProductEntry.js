c4dcf80b193785c3743f7b15d540af2b3d9f8053
Prabhjot Benipal <t8sipr00@students.oamk.fi>
2019-11-19 11:58:14 +0200

    Changed layout for basket page

--- front-end/src/components/BasketProductEntry.js
+++ front-end/src/components/BasketProductEntry.js
@@ -1,15 +1,32 @@
 import React from "react";
 import styles from "../CSS/BasketProductEntry.module.css";
+import Dropdown from "react-dropdown";
 export default props => {
   const data = props.data;
-
+  const quantityDropDownOptions = [...Array(10)].map((v, i) => {
+    console.log(i);
+    return i;
+  });
   return (
     <div className={styles.main}>
-      <div className={styles.name}>{data.name}</div>
-      <div className={styles.description}>{data.description}</div>
-      <div className={styles.priceAndQuantity}>
-        <div className={styles.price}>{`Price: ${data.price}`}</div>
-        <div className={styles.quantity}>{`Quantity: ${data.quantity}`}</div>
+      <div className={styles.productImage}></div>
+      <div className={styles.info}>
+        <div className={styles.name}>{data.name}</div>
+        <div className={styles.description}>{data.description}</div>
+        <div className={styles.priceAndQuantity}>
+          <div className={styles.price}>{`${data.price} euros`}</div>
+          <div className={styles.quantity}>
+            <Dropdown
+              options={quantityDropDownOptions}
+              value={quantityDropDownOptions[0]}
+              placeholder="Quantity"
+            />
+          </div>
+          <div className={styles.deleteButton}>{`Delete`}</div>
+          <div
+            className={styles.showMoreLikeThisButton}
+          >{`Show more like this`}</div>
+        </div>
       </div>
     </div>
   );
