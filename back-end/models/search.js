b0176dffa42499a0593887c191c9501b9f0d14ed
Long <longlong2000hcm@gmail.com>
2019-11-20 09:20:02 +0200

    temporary save

--- back-end/models/search.js
+++ back-end/models/search.js
@@ -1,20 +1,51 @@
 var db = require('../database');
 var knex = require("../database/database");
 
+function isTag(tag, callback) {
+  return knex
+    .from('tags')
+    .select()
+    .where("nameOfTag", tag)
+    .then(data => {
+      callback.then(data);
+    })
+    .catch(err => {
+      callback.catch(err);
+    });
+}
+
+function logSomething() {
+  return ("something");
+}
+
 var search = {
-    searchKeyWord: async function (keyword, callback) {
-        console.log("keyword: ", keyword)
-        return knex
-          .from('products')
-          .select()
-          .where("name", "like", `%${keyword}%`)
-          .then(data => {
-            callback.then(data);
-          })
-          .catch(err => {
-            callback.catch(err);
-          });
-      }
+  searchKeyWord:
+
+    /* function (word) {
+      return knex
+      .from("tags")
+      .select()
+      .where("nameOfTag", word);
+    } */
+
+    async function (keyword, callback) {
+      var category = [];
+      var tags = await isTag(keyword);
+      console.log("keyword: ", keyword);
+      console.log(tags);
+      return ({});
+
+      /* return knex
+        .from('products')
+        .select()
+        .where("name", "like", `%${keyword}%`)
+        .then(data => {
+          callback.then(data);
+        })
+        .catch(err => {
+          callback.catch(err);
+        }); */
+    }
 }
 
 module.exports = search;
