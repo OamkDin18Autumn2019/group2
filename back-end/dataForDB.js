d3776e6b67dd4f8a1788e1806dc1c1b6b1ceab35
Dmitrii Kislenko <kislenko1910@mail.ru>
2019-11-26 21:58:44 +0200

    Added some data for DB

---
+++ back-end/dataForDB.js
@@ -0,0 +1,15 @@
+//Add the data into the array to send it via postman when needed 
+[
+
+	{
+        "idUser": 1,
+        "name":"Nike boots",
+        "description": "Normal Nike boots for running",
+        "tags":"boots,nike",
+          "category":"shoes",
+        "amountOfProduct":"1",
+        "images":"https://kickster.ru/wp-content/uploads/2016/05/kickster_ru_nike_what_the_03.jpg"
+    },
+      
+      
+]
