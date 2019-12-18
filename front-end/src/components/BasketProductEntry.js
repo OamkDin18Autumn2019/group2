55b55365309da9c1eb0154d7d979eee7759b1690
Long <longlong2000hcm@gmail.com>
2019-12-18 09:35:02 +0200

    remove warnings part 2

--- front-end/src/components/BasketProductEntry.js
+++ front-end/src/components/BasketProductEntry.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from "../CSS/BasketProductEntry.module.css";
-import { Link } from 'react-router-dom';
+//import { Link } from 'react-router-dom';
 
 
 export default function BasketProductEntry(props) {
